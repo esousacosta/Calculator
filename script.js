37a89fc0079018cef4d8af80f19c6f5ca07e7dc1
esousacosta <victoremanoel23@gmail.com>
2022-05-18 21:38:40 +0200

    Create calculator class and wired addition button to its function

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 "use strict"
 
+class Calculator {
+    #displayElement;
+    displayValue;
+    constructor(ioDisplayElement)
+    {
+        this.displayValue = 0;
+        this.#displayElement = ioDisplayElement;
+    }
+
+    setDisplayValue(newDisplayValue)
+    {
+        this.displayValue = newDisplayValue;
+        this.#displayElement.textContent = this.displayValue;
+    }
+}
+
 function add(firstOperand, secondOperand)
 {
     return firstOperand + secondOperand;
@@ -25,3 +41,13 @@ function getRemainder(firstOperand, secondOperand)
 {
     return firstOperand % secondOperand;
 }
+
+const aCalculatorDisplay = document.getElementById("calculatorDisplayId");
+const aCalculatorDisplayParagraph = aCalculatorDisplay.firstChild;
+
+const aCalculator = new Calculator(aCalculatorDisplayParagraph);
+
+const aAdditionButton = document.getElementById("additionButtonId");
+aAdditionButton.addEventListener('click', () => aCalculator.setDisplayValue(add(1, 2)));
+
+
