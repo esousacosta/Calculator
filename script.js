9a1a678960fa9a4f806db0b97ad73d68ad02a1cd
esousacosta <victoremanoel23@gmail.com>
2022-05-18 21:20:17 +0200

    Re-style the calculator display and added the basic functions

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+function add(firstOperand, secondOperand)
+{
+    return firstOperand + secondOperand;
+}
+
+function subtract(firstOperand, secondOperand)
+{
+    return firstOperand - secondOperand;
+}
+
+function multiply(firstOperand, secondOperand)
+{
+    return firstOperand * secondOperand;
+}
+
+function divide(numerator, denominator)
+{
+    // TODO: check if the denominator is not zero
+    return numerator * denominator;
+}
+
+function getRemainder(firstOperand, secondOperand)
+{
+    return firstOperand % secondOperand;
+}
